9909c3e91341554e168d436ce2fa913f4722b4fa
usernameRaul <lopezjo299@gmail.com>
2021-12-19 23:53:21 -0600

    08-nov Creación del tablero con sus respectivas imagenes

--- Partida.xaml.cs
+++ Partida.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Cliente.ChessService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,15 +19,21 @@ namespace Cliente
     /// <summary>
     /// Interaction logic for Partida.xaml
     /// </summary>
-    public partial class Partida : Window
+    /// 
+    public partial class Partida : Window, IMatchServiceCallback
     {
         public int idUser;
         public string rival;
         public string MatchCode;
         public bool white;
+        public MatchServiceClient server_match;
+
         public Partida(int idUser_, string rival_, string MatchCode_, bool white_)
         {
             InitializeComponent();
+
+            InstanceContext instanceContext = new InstanceContext(this);
+            server_match = new MatchServiceClient(instanceContext);
 
             idUser = idUser_;
             rival = rival_;
@@ -35,6 +43,8 @@ namespace Cliente
             string color = (white) ? "whites" : "Blacks";
 
             prueba.Content += " " + rival + " you are "+color;
+
+            server_match.sendConnection(white, MatchCode);
         }
 
         private void BtnSend_Click(object sender, RoutedEventArgs e)
@@ -45,6 +55,11 @@ namespace Cliente
         private void BtnRendirse_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        public void ReciveMessage(string message)
+        {
+            
         }
     }
 }
--- obj/Debug/Cliente_Content.g.i.cs
+++ obj/Debug/Cliente_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/mainchess.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/fondo.png")]
 
 
